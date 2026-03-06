3cbeba62dab13dd64d10405ceaa55c366392e13f
anames2323 <137616037+anames2323@users.noreply.github.com>
2026-03-06 10:37:56 +1000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "8335567054:AAFi-byE9cKXUulQd3j0-cGekNol1Q2d-Ng"  # токен ботика
+CHANNEL_URL = "https://t.me/kanal1winmines"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1003887970360"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1003862464540"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = ""
+ADMIN_ID = 8245388091 # Ваш айди без скобочек
+PROMO = "1winminessignal" # промокод
