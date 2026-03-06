3be2a7cbb4f7a5243a549e6c0c856a0fd66d9378
anames2323 <137616037+anames2323@users.noreply.github.com>
2026-03-06 10:43:54 +1000

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 CHANNEL_URL = "https://t.me/kanal1winmines"  # ссылка канала проверка подписки
 CHANNEL_ID = "-1003887970360"  # айди канала для проверки подписки
 VERIF_CHANNEL_ID = "-1003862464540"  # айди чата для верификации(обсуждение канала с постбеками)
-SUPP = ""
+SUPP = "@pauIIanders"
 ADMIN_ID = 8245388091 # Ваш айди без скобочек
 PROMO = "1winminessignal" # промокод
+
